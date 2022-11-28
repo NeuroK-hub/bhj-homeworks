6202d37543f692fbdecf5ba1786b6d990bb50c85
NeuroK-hub <il_kuzmin@mail.ru>
2022-11-28 20:22:29 +0300

    first commit

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,13 +1,11 @@
-const divText = document.querySelectorAll('.reveal');
+const divText = Array.from(document.querySelectorAll('.reveal'));
 
-window.addEventListener('scroll', function() {
-    for (let item of divText) {
-        const ads = {top, bottom} = item.getBoundingClientRect();
-        if ((ads.top >= 0) && (ads.bottom >= 0)) {
-            item.classList.add('reveal_active');
-        } 
-        else {
-            item.classList.remove('reveal_active');
-        }
-    }
-});
+setInterval(() => {
+    divText.forEach((e) => {
+        if(e.getBoundingClientRect().bottom > 0 && e.getBoundingClientRect().top < window.innerHeight) {
+            e.classList.add('reveal_active');
+        } else {
+            e.classList.remove('reveal_active');
+        };
+    })
+})
